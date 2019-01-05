bf110e7714c10bafc7c1d2fc9910f8010433e8ae
Ravin Chhatrapati <ravinchhatrapati95@gmail.com>
2019-01-05 00:58:54 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,7 +4,7 @@ attr_accessor :word
 def initialize(word)
   @word = word
 end
-def search(arrayofwords)
+def match(arrayofwords)
   arrayofwords.select do |element|
   (@word.split("").sort) == (element.split("").sort)
 
